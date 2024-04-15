4bb7418324e7eef1399aa485b4a092b30c92ddb9
Kristof Poszet <kristof_poszet@epam.com>
2024-04-15 16:09:15 +0200

    fix: api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://et30ltpo5a.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://pxlb0mhlb8.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://lx70owfcvd.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://32f42qv6b4.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
